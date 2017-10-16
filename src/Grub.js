a1f43ac7d010b359129950a70c1bca5444cf2b44
Aric Alves <aric.alves@yahoo.com>
2017-10-16 11:38:17 -0700

    Finish beesbeesbees (inb4 mega commit)

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,10 @@
 var Grub = function() {
+	this.age = 0;
+	this.color = 'pink';
+	this.food = 'jelly';
+};
+
+Grub.prototype.eat = function(food) {
+	food === this.food ? console.log('nom nom nom.... nom') : console.log('Is this gluten free? Pass.');
 };
 
